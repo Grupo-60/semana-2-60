1652a537ccdf6ab49d3432459b1d2e05f174318c
juanchotrox <juanchotrox@hotmail.com>
2020-12-05 22:45:06 -0500

    adicion de los cambios realizados por Juan Alvarez

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,103 @@
+<template>
+  <footer class="page-footer bg-secondary text-white pt-4">
+    <!-- Footer Text -->
+    <div class="container-fluid text-center text-md-left">
+      <!-- Grid row -->
+      <div class="row">
+        <!-- Grid column -->
+        <div class="col-md-6 mt-md-0 mt-3">
+          <!-- Content -->
+            <div>
+              <h3 class="text-uppercase font-weight-italic">Phyzer</h3>
+              <p>
+                Queremos agradecerte por venir y observar todo lo que nuestra compañía está realizando para poder mejorar la calidad de vida y hacer que el día a día de las personas 
+                que usan nuestros productos sea un recuerdo vivido a plenitud.
+              </p>
+            <div>
+              <i class="fas fa-phone-alt"></i><span class="align-middle">+57 123 4321</span>
+            </div>
+
+            <div>
+              <i class="fas fa-envelope"></i><span class="align-middle"> phyzer-labs@domain.com</span>
+            </div>
+
+            <div>
+              <i class="fas fa-search-location"></i>
+              <span class="align-middle"> Calle 123 #45-67 Cali, Colombia</span>
+            </div>
+              
+              <div class="btn-group mr-2" role="group" aria-label="First button">
+                <a href="https://facebook.com" target="_blank" class="btn btn-primary btn-lg"><i class="fab fa-facebook-f"></i></a>
+              </div>
+              <div class="btn-group mr-2" role="group" aria-label="Second button">
+                <a href="https://twitter.com" target="_blank" class="btn btn-info btn-lg"><i class="fab fa-twitter"></i></a>
+              </div>
+              <div class="btn-group mr-2" role="group" aria-label="Second button">
+                <a href="https://pinterest.com" target="_blank" class="btn btn-danger btn-lg"><i class="fab fa-pinterest-p"></i></a>
+              </div>
+              <div class="btn-group mr-2" role="group" aria-label="Second button">
+                <a href="https://linkedin.com" target="_blank" class="btn btn-light btn-lg"><i class="fab fa-linkedin"></i></a>
+              </div>
+              <div class="btn-group mr-2" role="group" aria-label="Second button">
+                <a href="https://github.com/Grupo-60/semana-2-60" target="_blank" class="btn btn-dark btn-lg"><i class="fab fa-github"></i></a>
+              </div>
+
+            </div>
+        </div>
+        <!-- Grid column -->
+
+        <hr class="clearfix w-100 d-md-none pb-3" />
+
+        <!-- Grid column -->
+        <div class="col-md-6 mb-md-0 mb-3">
+          <!-- Content -->
+          <div>
+              <h3 class="text-uppercase font-weight-italic">Grupo 60</h3>
+            <div>
+              <i class="fas fa-user-alt"></i><span class="align-middle"> Juan Alvarez</span>
+            </div>
+            <div>
+              <i class="fas fa-envelope"></i><a href="mailto:juanchotrox@hotmail.com">  juanchotrox@hotmail.com</a>
+            </div>
+            <div>
+              <i class="fas fa-user-alt"></i><span class="align-middle"> Santiago Sanchez</span>
+            </div>
+            <div>
+              <i class="fas fa-envelope"></i><a href="mailto:sssalazar11@gmail.com">  sssalazar11@gmail.com</a>
+            </div>
+            <div>
+              <i class="fas fa-user-alt"></i><span class="align-middle"> Gaovani Rincon</span>
+            </div>
+            <div>
+              <i class="fas fa-envelope"></i><a href="mailto:georincon@hotmail.com">  georincon@hotmail.com</a>
+            </div>
+            <div>
+              <i class="fas fa-user-alt"></i><span class="align-middle"> Gaovani Rincon</span>
+            </div>
+            <div>
+              <i class="fas fa-envelope"></i><a href="mailto:danielgeorincon@gmail.com">  danielgeorincon@gmail.com</a>
+            </div>
+          </div>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+    <!-- Footer Text -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-1 bg-dark">
+      © 2020 Copyright: Phyzer Industries Colombia
+    </div>
+    <!-- Copyright -->
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "PageFooter",
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/TeamCard.vue
@@ -0,0 +1,29 @@
+<template>
+
+
+    <div class="card text-white bg-dark">
+        <div class="d-flex justify-content-center p-2">
+            <img v-bind:src="member.imagen" alt="Fotografia Miembro del Equipo">
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">{{ member.rol }}</h5>
+            <h6 class="card-text">{{ member.nombre }}</h6>           
+        </div>
+        <div class="card-footer">
+            <small class="card-text">{{ member.descripcion }}</small>
+        </div>
+    </div>
+
+
+</template>
+
+<script>
+    export default {
+        name: "TeamCard",
+        props: ['member']
+    }
+</script>
+
+<style scoped>
+
+</style>
